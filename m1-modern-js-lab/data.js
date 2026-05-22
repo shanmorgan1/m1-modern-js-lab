64c38e74793f6a1f04c4a186fc726ede31082490
shanmorgan1 <102261468+shanmorgan1@users.noreply.github.com>
2026-05-21 20:33:44 -0400

    Add files via upload

--- m1-modern-js-lab/data.js
+++ m1-modern-js-lab/data.js
@@ -1,5 +1,5 @@
 export const users = [
-  { id: 1, name: "Ava Carter", email: "ava@email.com" },
-  { id: 2, name: "Liam Chen", email: "liam@email.com" },
-  { id: 3, name: "Noah Patel", email: "noah@email.com" }
+  { id: 1, name: "Shannon Rifkin", email: "shannon@email.com" },
+  { id: 2, name: "Benjamin Rifkin", email: "ben@email.com" },
+  { id: 3, name: "Cheddar Rifkin", email: "cheddar@email.com" }
 ];
